dd5b0ff7bf339e4747ca27100959afea2df321db
Nick <nst447@gmail.com>
2019-12-05 17:02:20 -0800

    Added conditional to login button display

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
+import { isLoggedIn } from '../utils/auth';
 
 import Amplify from 'aws-amplify'
 import config from '../aws-exports'
@@ -11,7 +12,7 @@ const IndexPage = () => (
   <Layout>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site with multi-user authentication powered by <a href="https://amplify.aws">AWS Amplify</a></p>
-    <Link to="/app/login">Sign In</Link><br />
+    {!isLoggedIn() && <Link to="/app/login">Sign In</Link><br />}
     <Link to="/app/home">Home</Link><br />
     <Link to="/app/profile">Your profile</Link>
   </Layout>
