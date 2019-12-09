5ef2c7a6b47314dd308006b1ba5098bd40551b4c
Nick <nst447@gmail.com>
2019-12-09 14:44:19 -0800

    Fixing redirect bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,17 @@
 import React from 'react'
 import { navigate } from '@reach/router'
-import Layout from '../components/layout/layout'
 import { isLoggedIn } from '../utils/auth'
 
 import Amplify from 'aws-amplify'
 import config from '../aws-exports'
-import Login from '../components/Login';
 Amplify.configure(config)
 
 const IndexPage = () => {
-  if (isLoggedIn()) {
-    navigate("/app/home");
+  if (!isLoggedIn()) {
+    navigate("/app/login");
   }
 
-  return (
-    <Layout>
-      {!isLoggedIn() && <Login />}
-    </Layout>
-  )
+  return navigate("/app/home");
 }
 
 export default IndexPage
