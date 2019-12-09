d5f34a8ac337277ab4d2c18378dffcbd991a0bb3
Nick <nst447@gmail.com>
2019-12-09 10:42:59 -0800

    Added header to home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,13 @@ import { isLoggedIn } from '../utils/auth';
 
 const Home = () => <div>
   {isLoggedIn && (
-    <ul><li><Link to="/app/thursday-am">Day 1 - Thursday, Dec 13, 2018 AM</Link></li>
-    <li><Link to="/app/thursday-pm">Day 1 - Thursday, Dec 13, 2018 PM</Link></li>
-    <li><Link to="/app/friday">Day 2 - Friday, Dec 14, 2018 AM-PM</Link></li>
-    <li><Link to="/app/nurse-tech">Doris C. Barnie GI Nurses and Associates Course</Link></li></ul>
+    <>
+      <h2>42nd Annual New York Course</h2>
+      <ul><li><Link to="/app/thursday-am">Day 1 - Thursday, Dec 13, 2018 AM</Link></li>
+        <li><Link to="/app/thursday-pm">Day 1 - Thursday, Dec 13, 2018 PM</Link></li>
+        <li><Link to="/app/friday">Day 2 - Friday, Dec 14, 2018 AM-PM</Link></li>
+        <li><Link to="/app/nurse-tech">Doris C. Barnie GI Nurses and Associates Course</Link></li></ul>
+    </>
   )}
 </div>
 
