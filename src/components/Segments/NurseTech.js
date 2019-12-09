a4bbf3a568de21879b1c1922dfb44593b917d1db
Nick <nst447@gmail.com>
2019-12-09 09:03:40 -0800

    Changed header for Nurse Tech

--- src/components/Segments/NurseTech.js
+++ src/components/Segments/NurseTech.js
@@ -5,7 +5,9 @@ import TalksList from '../TalksList'
 const NurseTech = () => {
   return (
     <>
-      <h2>Nurse Tech<span style={{ float: 'right' }}><Link to="/">home</Link></span></h2>
+      <h2>Doris C. Barnie GI Nurses and Associates Course
+        <span style={{ float: 'right' }}><Link to="/">home</Link></span>
+      </h2>
       <TalksList track="Nurse-Tech" />
     </>
   )
