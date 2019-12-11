74bd743284d3e3364fd83cf769d734449badb87d
Nick <nst447@gmail.com>
2019-12-10 16:53:55 -0800

    Changed timeframe

--- src/components/EpicSec/SegmentsDisplay/EpicFriday.js
+++ src/components/EpicSec/SegmentsDisplay/EpicFriday.js
@@ -6,7 +6,7 @@ const EpicFriday = () => {
   return (
     <>
       <h2>Friday<span style={{ float: 'right' }}><Link to="/app/home">back</Link></span></h2>
-      <EpicTalksList timeframe="121400-121424" />
+      <EpicTalksList timeframe="050300-050324" />
     </>
   )
 }
